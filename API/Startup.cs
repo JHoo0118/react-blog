9bf57775f32774d42c29ebad21c76cc76ad1c060
JHoo0118 <kik3078@gmail.com>
2020-08-23 03:13:28 +0900

    Edit CSP settings

--- API/Startup.cs
+++ API/Startup.cs
@@ -139,7 +139,7 @@ namespace API
             app.UseReferrerPolicy(opt => opt.NoReferrer());
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
-            app.UseCspReportOnly(opt => opt
+            app.UseCsp(opt => opt
                     .BlockAllMixedContent()
                     .StyleSources(s => s.Self()
                         .CustomSources("https://fonts.googleapis.com", "https://use.fontawesome.com",
@@ -151,6 +151,11 @@ namespace API
                     .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com", "blob:", "data:"))
                     .ScriptSources(s => s.Self().CustomSources("sha256-ma5XxS1EBgt17N22Qq31rOxxRWRfzUTQS1KOtfYwuNo="))
                 );
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Feature-Policy", "geolocation 'none';midi 'none';sync-xhr 'none';microphone 'none';camera 'none';magnetometer 'none';gyroscope 'none';fullscreen 'self';payment 'none';");
+                await next.Invoke();
+            });
 
             // app.UseHttpsRedirection();
 
